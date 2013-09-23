b6e7c27181d422a2b878583799a78c85b629702d
Luan fonseca de farias <luanfonceca@gmail.com>
2013-09-23 18:21:15 -0300

    Adding all the initial files and configs of our Django project.

---
+++ econet/accounts/urls.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+u"""
+user.py
+
+Criado por Luan Fonseca <luanfonceca@gmail.com> em 23/09/2013.
+"""
+
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('accounts.views',
+    # Write here yours URLs
+)
---
+++ econet/utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+utils.py
+
+Criado por Luan Fonseca <luanfonceca@gmail.com> em 17/06/2013.
+"""
+
+from django.utils import timezone
+from django.core.mail import send_mail
+from django import forms
+
+
+def now():
+    return timezone.now()
+
+
+class HistoryModelForm(forms.ModelForm):
+    def save(self, user=None, *args, **kwargs):
+        if user:
+            self.instance.updated_by = user
+            if not self.instance.pk:
+                self.instance.created_by = self.instance.updated_by
+        return super(HistoryModelForm, self).save(*args, **kwargs)
