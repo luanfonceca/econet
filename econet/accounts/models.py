451ab7fbd3d17a44414c2da3e4ef4b685cb85c06
Luan fonseca de farias <luanfonceca@gmail.com>
2013-10-17 13:37:57 -0300

    Adicionando traducao de alguns textos e corrigindo o Models de User.

--- econet/accounts/models.py
+++ econet/accounts/models.py
@@ -14,38 +14,33 @@ from django.contrib.auth.models import (AbstractBaseUser,
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        _now = now()
+
+    def _create_user(self, email, password,
+                     is_staff, is_superuser, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        now = timezone.now()
         if not email:
-            raise ValueError(_(u'The given email must be set.'))
-        email = UserManager.normalize_email(email)
-        user = self.model(
-            email=email,
-            is_staff=True,
-            is_active=True,
-            is_superuser=False,
-            last_login=_now,
-            created_at=_now,
-            **extra_fields
-        )
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email,
+                          is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser, last_login=now,
+                          date_joined=now, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False,
+                                 **extra_fields)
+
     def create_superuser(self, email, password, **extra_fields):
-        user = self.create_user(
-            email,
-            password,
-            **extra_fields
-        )
-        user.is_staff = True
-        user.is_active = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
+        return self._create_user(email, password, True, True,
+                                 **extra_fields)
 
-
-class User(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser):
     first_name = models.CharField(
         null=True,
         blank=True,
